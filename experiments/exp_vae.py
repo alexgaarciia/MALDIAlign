82322dda5ee4d8eafb6c4f0d0cfc97049e63be29
agnavarr <agnavarr@tsc.uc3m.es>
2025-12-10 16:03:08 +0100

    upload vae exps, multidecoder and multidecoder w/ coral

--- experiments/exp_vae.py
+++ experiments/exp_vae.py
@@ -11,12 +11,10 @@ sys.path.append(project_root)
 # ---------------------------
 # Imports
 # ---------------------------
-import pickle
 import torch
-from torch.utils.data import TensorDataset, DataLoader
-from models.deep.VAE import VAE_Extended
+from models.deep.VAEBernoulli import VAE_Bernoulli_Extended
 from utils.load_config import load_config
-from utils.load_data import load_driams, map_domains, minmax_spectrum, construct_dataloaders
+from utils.load_data import load_driams, map_domains, construct_dataloaders
 from utils.viz import *
 from utils.eval import eval_model
 from sklearn.model_selection import train_test_split
@@ -38,7 +36,7 @@ def main():
     # ---------------------------
     print("===== Loading and preparing data... =====")
     cfg = load_config()
-    driams_pkl = cfg["data"]["DRIAMS_REDUCED_PKL"]
+    driams_pkl = cfg["data"]["DRIAMS_REDUCED_PKL2"]
     driams_filtered= load_driams(driams_pkl, filter=["DRIAMS_A", "DRIAMS_D"])
     dataA, labelA, metaA = driams_filtered["DRIAMS_A"]["data"], driams_filtered["DRIAMS_A"]["label"], driams_filtered["DRIAMS_A"]["meta"]
     dataD, labelD, metaD = driams_filtered["DRIAMS_D"]["data"], driams_filtered["DRIAMS_D"]["label"], driams_filtered["DRIAMS_D"]["meta"]
@@ -57,66 +55,54 @@ def main():
     label_final = np.concatenate([labelA_sub, labelD])
     meta_final  = pd.concat([metaA_sub, metaD], ignore_index=True)
 
+    # Row-wise min-max normalization
+    data_norm = (data_final - data_final.min(axis=1, keepdims=True)) / (
+        data_final.max(axis=1, keepdims=True) - data_final.min(axis=1, keepdims=True) + 1e-8
+    )
+
     # Construct dataloaders
     domain_ids = map_domains(meta_final)
-    X_train, X_val, y_train, y_val, domain_train, domain_val = train_test_split(data_final, label_final, domain_ids, test_size=0.2, random_state=42, stratify=label_final)
+    X_train, X_val, y_train, y_val, domain_train, domain_val = train_test_split(data_norm, label_final, domain_ids, test_size=0.2, random_state=42, stratify=label_final)
 
-    # ---------------------------
-    # Scaling
-    # ---------------------------
-    scaler, X_train_scaled, X_val_scaled = scale_data(X_train, X_val)
-
-    with open(os.path.join(results_dir, "scaler.pkl"), 'wb') as file:
-        pickle.dump(scaler, file)
-
-    # ---------------------------
-    # Dataloaders
-    # ---------------------------
-    X_train_tensor = torch.tensor(X_train_scaled, dtype=torch.float32)
-    X_val_tensor   = torch.tensor(X_val_scaled, dtype=torch.float32)
+    X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+    X_val_tensor   = torch.tensor(X_val, dtype=torch.float32)
+    X_all_tensor = torch.tensor(data_norm, dtype=torch.float32)
 
     domain_train_tensor = torch.tensor(domain_train, dtype=torch.long)
     domain_val_tensor   = torch.tensor(domain_val, dtype=torch.long)
+    domain_all_tensor = torch.tensor(domain_ids, dtype=torch.long)
 
-    train_loader, val_loader = construct_dataloaders(X_train_tensor, X_val_tensor, domain_train_tensor, domain_val_tensor, batch_size=64)
+    train_loader, val_loader, all_loader = construct_dataloaders(X_train_tensor, X_val_tensor, X_all_tensor, domain_train_tensor, domain_val_tensor, domain_all_tensor, batch_size=64)
 
     # ---------------------------
     # Model
     # ---------------------------
-    print("===== Instantiating model.. =====")
-    model = VAE_Extended(X_train.shape[1], latent_dim=64, epochs=200, annealing_epochs=100, patience=40)
+    print("===== Instantiating model.. =====\n")
+    model = VAE_Bernoulli_Extended(X_train.shape[1], latent_dim=64, epochs=200, annealing_epochs=100, patience=40)
 
-    print("===== Training model... =====")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"===== Training model with {device} =====\n")
     model.trainloop(train_loader, val_loader, device=device)
 
     # ---------------------------
     # Store results
     # ---------------------------
-    print("===== Finished training, saving model and metrics... =====")
+    print("===== Finished training, saving model and metrics... =====\n")
     torch.save(model.state_dict(), os.path.join(results_dir, "vae.pth"))
     plot_model_metrics(model, "VAE", save=True, path=results_dir+"/vae_loss")
 
     # ---------------------------
     # t-SNEs
     # ---------------------------
-    print("===== Computing t-SNEs... =====")
-    X_all = minmax_spectrum(data_final)
-    X_all_tensor = torch.tensor(X_all, dtype=torch.float32)
-    domain_all_tensor = torch.tensor(domain_ids, dtype=torch.long)
-
-    all_dataset = TensorDataset(X_all_tensor, domain_all_tensor)
-    all_loader  = DataLoader(all_dataset, batch_size=256, shuffle=False)
-
+    print("====== Computing t-SNE... ======\n")
     mus_all = eval_model(model, all_loader, device, use_domain=False)
-
     tsne_df = compute_tsne_df(mus_all, label_final, meta_final)
+    df_all, tsne_results = compute_tsne_per_species(mus_all, label_final, meta_final)
+
+    print("====== Saving plots... ======\n")
     plot_tsne_global(tsne_df, per_species=False, overlay_per_hospital=False, save=True, path=results_dir + "/vae_tsne_global.png")
     plot_tsne_global(tsne_df, per_species=True, overlay_per_hospital=False, save=True, path=results_dir + "/vae_tsne_global_species.png")
     plot_tsne_global(tsne_df, per_species=True, overlay_per_hospital=True, save=True, path=results_dir + "/vae_tsne_global_species_overlay.png")
-
-    # t-SNE for each species
-    df_all, tsne_results = compute_tsne_per_species(mus_all, label_final, meta_final)
     plot_tsne_species(df_all, tsne_results, overlay_per_hospital=False, save=True, path=results_dir + "/vae_tsne_species.png")
     plot_tsne_species(df_all, tsne_results, overlay_per_hospital=True, save=True, path=results_dir + "/vae_tsne_species_overlay.png")
 
