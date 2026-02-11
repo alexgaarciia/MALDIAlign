d88b04f605f5b2cca6a4f252e52c7431a9c42220
agnavarr <agnavarr@tsc.uc3m.es>
2026-02-11 15:56:10 +0100

    reduced manual loading

--- experiments/finetuning/prepare_data.py
+++ experiments/finetuning/prepare_data.py
@@ -4,12 +4,9 @@
 import os
 import sys
 import pickle
-import numpy as np
-import pandas as pd
 from pathlib import Path
 from datetime import datetime
 
-# Find project root
 PROJECT_NAME = "MALDIAlign"
 cwd = Path().resolve()
 
@@ -31,7 +28,6 @@ print("Project root:", target)
 ############################
 # OUTPUT DIRECTORY
 ############################
-
 BASE_OUTPUT = Path(
     "/export/usuarios01/agnavarr/MALDIAlign/experiments/finetuning/output_data"
 )
@@ -48,7 +44,13 @@ print("Saving splits to:", experiment_dir)
 # IMPORTS
 ############################
 from utils.config import load_config
-from utils.data import load_pkl, subsample_dataset_stratified
+from utils.data import (
+    load_driams,
+    load_marisma,
+    load_msumg,
+    load_rki,
+    subsample_dataset_stratified,
+)
 
 
 ############################
@@ -56,61 +58,63 @@ from utils.data import load_pkl, subsample_dataset_stratified
 ############################
 cfg = load_config()
 
-driams_pkl = cfg["data"]["DRIAMS_REDUCED_PKL"]
-marisma_pkl = cfg["data"]["MARISMa_REDUCED_PKL"]
-msumg_pkl = cfg["data"]["MSUMG_PKL"]
-rki_pkl = cfg["data"]["RKI_PKL"]
+# Load datasets
+driams_dict  = load_driams(cfg["data"]["DRIAMS_REDUCED_PKL"])
+marisma_dict = load_marisma(cfg["data"]["MARISMa_REDUCED_PKL"])
+msumg_dict   = load_msumg(cfg["data"]["MSUMG_PKL"])  # already filters agar
+rki_dict     = load_rki(cfg["data"]["RKI_PKL"])
 
-driams = load_pkl(driams_pkl)
-marisma = load_pkl(marisma_pkl)
-msumg = load_pkl(msumg_pkl)
-rki = load_pkl(rki_pkl)
+# Unpack
+data_driams, label_driams, meta_driams = (
+    driams_dict["data"],
+    driams_dict["label"],
+    driams_dict["meta"],
+)
 
-data_driams, label_driams, meta_driams = driams["data"], driams["label"], pd.DataFrame.from_records(list(driams["meta"]))
-data_marisma, label_marisma, meta_marisma = marisma["data"], marisma["label"], pd.DataFrame.from_records(list(marisma["meta"]))
-data_msumg, label_msumg, meta_msumg = msumg["data"], msumg["label"], pd.DataFrame.from_records(list(msumg["meta"]))
+data_marisma, label_marisma, meta_marisma = (
+    marisma_dict["data"],
+    marisma_dict["label"],
+    marisma_dict["meta"],
+)
 
-if "agar_type" in meta_msumg.columns:
-    mask_agar = meta_msumg["agar_type"] == "agar"
+data_msumg, label_msumg, meta_msumg = (
+    msumg_dict["data"],
+    msumg_dict["label"],
+    msumg_dict["meta"],
+)
 
-    data_msumg = data_msumg[mask_agar.values]
-    label_msumg = label_msumg[mask_agar.values]
-    meta_msumg = meta_msumg.loc[mask_agar].reset_index(drop=True)
-    
-data_rki, label_rki, meta_rki = rki["data"], rki["label"], pd.DataFrame.from_records(list(rki["meta"]))
+data_rki, label_rki, meta_rki = (
+    rki_dict["data"],
+    rki_dict["label"],
+    rki_dict["meta"],
+)
 
-meta_marisma.insert(0, "hospital", "MARISMA")
-meta_msumg.insert(0, "hospital", "MS-UMG")
-meta_rki.insert(0, "hospital", "RKI")
+# Split DRIAMS by hospital using masks
+maskA = meta_driams["hospital"] == "DRIAMS_A"
+maskB = meta_driams["hospital"] == "DRIAMS_B"
+maskC = meta_driams["hospital"] == "DRIAMS_C"
+maskD = meta_driams["hospital"] == "DRIAMS_D"
+
+dataA, labelA, metaA = data_driams[maskA], label_driams[maskA], meta_driams[maskA]
+dataB, labelB, metaB = data_driams[maskB], label_driams[maskB], meta_driams[maskB]
+dataC, labelC, metaC = data_driams[maskC], label_driams[maskC], meta_driams[maskC]
+dataD, labelD, metaD = data_driams[maskD], label_driams[maskD], meta_driams[maskD]
 
 print("\n===== DATA LOADED =====")
-
-# Filter the data by hospital
-filtered_data = {}
-for hosp in meta_driams["hospital"].unique():
-    idx = np.where(meta_driams["hospital"].values == hosp)[0]
-    filtered_data[hosp] = {
-        "data": data_driams[idx],
-        "label": label_driams[idx],
-        "meta": meta_driams.iloc[idx]
-    }
-
-dataA, labelA, metaA = filtered_data["DRIAMS_A"]["data"], filtered_data["DRIAMS_A"]["label"], filtered_data["DRIAMS_A"]["meta"]
-dataB, labelB, metaB = filtered_data["DRIAMS_B"]["data"], filtered_data["DRIAMS_B"]["label"], filtered_data["DRIAMS_B"]["meta"]
-dataC, labelC, metaC = filtered_data["DRIAMS_C"]["data"], filtered_data["DRIAMS_C"]["label"], filtered_data["DRIAMS_C"]["meta"]
-dataD, labelD, metaD = filtered_data["DRIAMS_D"]["data"], filtered_data["DRIAMS_D"]["label"], filtered_data["DRIAMS_D"]["meta"]
 
 
 ############################
 # STRATIFIED SUBSAMPLING
 ############################
 print("\n===== STRATIFIED SUBSAMPLING =====")
-A_split = subsample_dataset_stratified(dataA, labelA, metaA, n_samples=200)
-B_split = subsample_dataset_stratified(dataB, labelB, metaB, n_samples=200)
-C_split = subsample_dataset_stratified(dataC, labelC, metaC, n_samples=200)
-M_split = subsample_dataset_stratified(data_marisma, label_marisma, meta_marisma, n_samples=200)
-R_split = subsample_dataset_stratified(data_rki, label_rki, meta_rki, n_samples=200)
-D_split = subsample_dataset_stratified(dataD, labelD, metaD, n_samples=1000, ood=True)
+
+A_split  = subsample_dataset_stratified(dataA, labelA, metaA, n_samples=200)
+B_split  = subsample_dataset_stratified(dataB, labelB, metaB, n_samples=200)
+C_split  = subsample_dataset_stratified(dataC, labelC, metaC, n_samples=200)
+M_split  = subsample_dataset_stratified(data_marisma, label_marisma, meta_marisma, n_samples=200)
+R_split  = subsample_dataset_stratified(data_rki, label_rki, meta_rki, n_samples=200)
+
+D_split  = subsample_dataset_stratified(dataD, labelD, metaD, n_samples=1000, ood=True)
 MS_split = subsample_dataset_stratified(data_msumg, label_msumg, meta_msumg, n_samples=1000, ood=True)
 
 
@@ -121,13 +125,20 @@ splits_idx = {
     "DRIAMS_A": {"finetuning": A_split["finetuning"]["idx"]},
     "DRIAMS_B": {"finetuning": B_split["finetuning"]["idx"]},
     "DRIAMS_C": {"finetuning": C_split["finetuning"]["idx"]},
-    "MARISMA": {"finetuning": M_split["finetuning"]["idx"]},
-    "RKI": {"finetuning": R_split["finetuning"]["idx"]},
-    "DRIAMS_D": {"finetuning": D_split["finetuning"]["idx"], "test": D_split["test"]["idx"]},
-    "MS-UMG": {"finetuning": MS_split["finetuning"]["idx"], "test": MS_split["test"]["idx"]}
+    "MARISMA":  {"finetuning": M_split["finetuning"]["idx"]},
+    "RKI":      {"finetuning": R_split["finetuning"]["idx"]},
+    "DRIAMS_D": {
+        "finetuning": D_split["finetuning"]["idx"],
+        "test": D_split["test"]["idx"],
+    },
+    "MS-UMG": {
+        "finetuning": MS_split["finetuning"]["idx"],
+        "test": MS_split["test"]["idx"],
+    },
 }
 
 print("\n===== SAVING SPLITS & DATA =====")
+
 with open(experiment_dir / "splits_idx.pkl", "wb") as f:
     pickle.dump(splits_idx, f)
 
